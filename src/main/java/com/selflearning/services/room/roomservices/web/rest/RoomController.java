c38a1dff901d2939660429f9d5594a6ff3c7bece
Noura Mozaffari <noura.mozaffari@gmail.com>
2020-03-26 03:00:39 +0430

    initial commit

---
+++ src/main/java/com/selflearning/services/room/roomservices/web/rest/RoomController.java
@@ -0,0 +1,32 @@
+package com.selflearning.services.room.roomservices.web.rest;
+
+import com.selflearning.services.room.roomservices.domain.RoomEntity;
+import com.selflearning.services.room.roomservices.repository.RoomRepository;
+import io.micrometer.core.instrument.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/rooms")
+public class RoomController {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<RoomEntity> findAll(@RequestParam(name = "roomNumber", required = false)String roomNumber) {
+
+        if (StringUtils.isNotEmpty(roomNumber)) {
+            return Collections.singletonList(roomRepository.findByRoomNumber(roomNumber));
+        }
+
+        return (List<RoomEntity>) roomRepository.findAll();
+
+    }
+}
